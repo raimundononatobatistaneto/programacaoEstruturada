526864aefc268f4977b1abf948f9a055268e038f
raimundononatobatistaneto <54728051+raimundononatobatistaneto@users.noreply.github.com>
2019-08-30 21:08:43 -0300

    Update questao01.cpp

--- lista01/questao01.cpp
+++ lista01/questao01.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main (){
 
     cout << "Oi Mundo!, Estou pronto para C++\n";
-    cout << "Hey, escrever std:: o tempo todo é chato d+\n";
+    cout << "Hey, escrever std:: o tempo todo é chato d+++++++++\n";
     cout << "Mas eu sei como evitar isso :)\n";
     return 0;
 }
