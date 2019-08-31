26abad9c6a16099fad98a834b962ff03d862f11c
IFPB <ifpb@MDK00LB10-JP>
2019-08-30 21:07:16 -0300

    criei lista01

---
+++ lista01/questao01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    cout << "Oi Mundo!, Estou pronto para C++\n";
+    cout << "Hey, escrever std:: o tempo todo é chato d+\n";
+    cout << "Mas eu sei como evitar isso :)\n";
+    return 0;
+}
